5e596e49b9e8aac49097c32cbebfd97f4f18530a
wassupiari <normannoiari@gmail.com>
2024-05-27 12:23:31 +0200

    id-1 resolved

--- src/it/unisa/control/HomeServlet.java
+++ src/it/unisa/control/HomeServlet.java
@@ -3,6 +3,8 @@ package it.unisa.control;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -19,45 +21,60 @@ import it.unisa.model.ProdottoDao;
  */
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		ProdottoDao dao = new ProdottoDao();
-		
-		ArrayList<ArrayList<ProdottoBean>> categorie = new ArrayList<>();
-		String redirectedPage = request.getParameter("page");
-		
-		try {
-			ArrayList<ProdottoBean> PS5 = dao.doRetrieveByPiattaforma("PlayStation 5");
-			ArrayList<ProdottoBean> XboxSeries = dao.doRetrieveByPiattaforma("Xbox Series");
-			ArrayList<ProdottoBean> Switch = dao.doRetrieveByPiattaforma("Nintendo Switch");
-			ArrayList<ProdottoBean> PS4 = dao.doRetrieveByPiattaforma("PlayStation 4");
-			ArrayList<ProdottoBean> Xone = dao.doRetrieveByPiattaforma("Xbox One");
-			
-			categorie.add(PS5);
-			categorie.add(XboxSeries);
-			categorie.add(Switch);
-			categorie.add(PS4);
-			categorie.add(Xone);
+    private static final long serialVersionUID = 1L;
+    
+    private static final List<String> BLACKLISTED_PATHS = Arrays.asList(
+        "META-INF/context.xml",
+        "WEB-INF/web.xml"
+    );
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProdottoDao dao = new ProdottoDao();
+        
+        ArrayList<ArrayList<ProdottoBean>> categorie = new ArrayList<>();
+        String redirectedPage = request.getParameter("page");
+        
+        try {
+            ArrayList<ProdottoBean> PS5 = dao.doRetrieveByPiattaforma("PlayStation 5");
+            ArrayList<ProdottoBean> XboxSeries = dao.doRetrieveByPiattaforma("Xbox Series");
+            ArrayList<ProdottoBean> Switch = dao.doRetrieveByPiattaforma("Nintendo Switch");
+            ArrayList<ProdottoBean> PS4 = dao.doRetrieveByPiattaforma("PlayStation 4");
+            ArrayList<ProdottoBean> Xone = dao.doRetrieveByPiattaforma("Xbox One");
+            
+            categorie.add(PS5);
+            categorie.add(XboxSeries);
+            categorie.add(Switch);
+            categorie.add(PS4);
+            categorie.add(Xone);
 
-			request.getSession().setAttribute("categorie", categorie);
-			
-
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + redirectedPage);
-		dispatcher.forward(request, response);
-	}
-
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		doGet(request, response);
-	}
-
+            request.getSession().setAttribute("categorie", categorie);
+            
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        
+        if (isBlacklistedPath(redirectedPage)) {
+            // Forzare un reindirizzamento verso un'altra pagina del sito
+            redirectedPage = "Home.jsp"; 
+        }
+        
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/" + redirectedPage);
+        dispatcher.forward(request, response);
+    }
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+    
+    private boolean isBlacklistedPath(String path) {
+        if (path == null) {
+            return true;
+        }
+        for (String blacklistedPath : BLACKLISTED_PATHS) {
+            if (path.contains(blacklistedPath)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
